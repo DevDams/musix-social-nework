3ef9c758ae56b84b45f753e074300808d76c4980
DevDams <ad.aimedesire@gmail.com>
2021-07-16 12:06:42 +0000

    modif layout and content design

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -4,19 +4,18 @@
     <!-- PAGE D'ACCUEIL -->
     <div class="home-content">
         <div class="home-left">
-            <img src="img/cover.png" alt="cover">
+            <img src="~/assets/images/intro-cover-1.jpeg" alt="cover">
         </div>
         <div class="home-right">
-            <img src="img/music-logo.png" alt="music-logo">
-            <h1>Welcome to the wonder wonderland</h1>
+            <h1>Enregistrez et partagez vos musique maintenant</h1>
             <h2>Rejoignez nous dès aujourd'hui.</h2>
             <div class="box-button">
-                <a href="signup.html">
-                    <button class="btn btn-blue">S'inscrire</button>
-                </a>
-                <a href="login.html">
-                    <button class="btn btn-light">Se connecter</button>
-                </a>
+              <nuxt-link to="/signup">
+                <button class="btn btn-blue">S'inscrire</button>
+              </nuxt-link>
+              <nuxt-link to="/login">
+                <button class="btn btn-light">Se connecter</button>
+              </nuxt-link>
             </div>
         </div>
     </div>
@@ -28,100 +27,92 @@ export default {}
 </script>
 
 <style scoped>
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-a{
-    text-decoration: none;
+a {
+  text-decoration: none;
 }
 
 /* PAGE D'ACCUEIL */
-.home-content{
-    display: flex;
-    align-items: center;
+.home-content {
+  display: flex;
+  align-items: center;
 }
 
-.home-left{
-    background: linear-gradient( #42ACF2,#B042F2);
-    padding: 50px;
-    width: 50%;
-    height: 100vh;
+.home-left {
+  background: linear-gradient( #42ACF2,#B042F2);
+  width: 50%;
+  height: 100vh;
 }
 
-.home-left img{
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
+.home-left img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
-.logo{
-    width: 50px;
-    height: 50px;
-    object-fit: cover;
+.home-right {
+  padding: 0 40px;
+  width: 50%;
 }
 
-.home-right{
-    padding: 0 20px;
-    width: 50%;
+.home-right img {
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
 }
 
-.home-right img{
-    width: 50px;
-    height: 50px;
-    object-fit: cover;
+.home-right h1 {
+  font-weight: 800;
+  font-size: 70px;
+  padding: 30px 0;
 }
 
-.home-right h1{
-    font-weight: 400;
-    font-size: 70px;
-    padding: 30px 0;
+.home-right h2 {
+  font-size: 34px;
+  padding-bottom: 40px;
+  font-weight: 400;
 }
 
-.home-right h2{
-    padding-bottom: 20px;
-    font-weight: 300;
+.box-button {
+  width: 300px;
 }
 
-.box-button{
-    width: 300px;
+.btn {
+  width: 100%;
+  border-radius: 25px;
+  -webkit-border-radius: 25px;
+  -moz-border-radius: 25px;
+  -ms-border-radius: 25px;
+  -o-border-radius: 25px;
+  padding: 13px;
+  margin: 10px 0;
+  font-size: 18px;
+  font-weight: bold;
+  cursor: pointer;
 }
 
-.btn{
-    width: 100%;
-    border-radius: 25px;
-    -webkit-border-radius: 25px;
-    -moz-border-radius: 25px;
-    -ms-border-radius: 25px;
-    -o-border-radius: 25px;
-    padding: 10px;
-    margin: 5px 0;
-    font-size: 15px;
-    font-weight: bold;
-    cursor: pointer;
+.btn-blue {
+  border: none;
+  color: white;
+  background: linear-gradient(90deg, #42ACF2,#B042F2);
 }
 
-.btn-blue{
-    border: none;
-    color: white;
-    background-color: #1A91DA;
-}
-
-.btn-blue:hover{
-    background-color: #1a83c4;
+.btn-blue:hover {
+  background-color: #1a83c4;
 }
 
 .btn-light{
-    border: 1px solid #1A91DA;
-    color: #1A91DA;
-    background-color: transparent;
+  border: 1px solid #42ACF2;
+  color: #42ACF2;
+  background-color: transparent;
 }
 
 .btn-light:hover{
-    background-color: #3bb4ff77;
+  background-color: #3bb4ff54;
 }
-
 </style>
--- frontend/pages/login/index.vue
+++ frontend/pages/login/index.vue
@@ -2,28 +2,27 @@
   <div>
   <!-- commencer à coder ici -->
     <!-- PAGE DE CONNECTION -->
-      <div class="login-contain">
-          <div class="log-content">
-              <img src="../img/music-logo.png" alt="music-logo" class="logo">
-              <h1>Se connecter</h1>
-              <form action="#" class="form">
-                  <div class="formcase">
-                      <input type="text" placeholder="Téléphone, email ou non d'utilisateur">
-                  </div>
-                  <div class="formcase">
-                      <input type="text" placeholder="Mot de passe">
-                  </div>
-              </form>
-              <div class="box-button">
-                  <a href="#">
-                      <button class="btn btn-blue">Se connecter</button>
-                  </a>
-                  <a href="#">Mot de passe oublié ?</a>
-                  <span>-</span>
-                  <a href="#">S'inscrire</a>
-              </div>
-          </div>
-      </div>
+    <div class="login-contain">
+        <div class="log-content">
+            <h1>Se connecter</h1>
+            <form action="#" class="form">
+                <div class="formcase">
+                    <input type="text" placeholder="Téléphone, email ou non d'utilisateur">
+                </div>
+                <div class="formcase">
+                    <input type="text" placeholder="Mot de passe">
+                </div>
+            </form>
+            <div class="box-button">
+                <a href="#">
+                    <button class="btn btn-blue">Se connecter</button>
+                </a>
+                <a href="#">Mot de passe oublié ?</a>
+                <span>-</span>
+                <a href="#">S'inscrire</a>
+            </div>
+        </div>
+    </div>
   </div>
 </template>
 
@@ -32,29 +31,22 @@ export default {}
 </script>
 
 <style scoped>
- *{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
-a{
+a {
   text-decoration: none;
 }
 
 .login-contain{
   max-width: 350px;
-  margin: 20px auto;
+  margin: 70px auto;
 }
 
-.logo{
-  width: 50px;
-  height: 50px;
-  object-fit: cover;
-}
-
-.formcase{
+.formcase {
   width: 100%;
   border: 1px solid gray;
   border-radius: 5px;
@@ -65,12 +57,13 @@ a{
   margin: 25px 0;
 }
 
-.formcase input{
+.formcase input {
   border: none;
   font-size: 18px;
   width: 100%;
   padding: 15px;
   outline-color: #1A91DA;
+  background: none;
 }
 
 .formcase input::placeholder{
@@ -88,9 +81,9 @@ a{
   -moz-border-radius: 25px;
   -ms-border-radius: 25px;
   -o-border-radius: 25px;
-  padding: 15px;
+  padding: 13px;
   margin-bottom: 20px;
-  font-size: 15px;
+  font-size: 18px;
   font-weight: bold;
   cursor: pointer;
 }
@@ -99,9 +92,11 @@ a{
   border: none;
   color: white;
   background: linear-gradient(to right, #42ACF2,#B042F2);
+  transition: .2s all ease-in-out;
 }
 
 .btn-blue:hover{
   background: linear-gradient(to left, #42ACF2,#B042F2);
+  transition: .2s all ease-in-out;
 }
 </style>
